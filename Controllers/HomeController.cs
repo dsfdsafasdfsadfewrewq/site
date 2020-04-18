db85f461f58d3ce7c8b7b813ba67f13ff6170f0f
alex <super99leo1@yandex.ru>
2020-04-18 23:30:32 +0300

    добавил список авто

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,11 +13,18 @@ namespace Сайт.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public List<ItemClass> items = new List<ItemClass>();
+
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
-        }
-
+            items.Add(new ItemClass("LAMBORGHINI HURACAN EVO", "/img/HUracan-Evo-310x152.png", 1096, "Желтый / Синий"));
+            items.Add(new ItemClass("Lamborghini Huracan Spyder LP 610", "/img/Spyder-310x121.png", 959, "Серебряный"));
+            items.Add(new ItemClass("McLaren 650S", "/img/BS5B3720-copy-310x116.png", 767, "Черный / Белый"));
+            items.Add(new ItemClass("Audi R8 Spyder", "/img/Audi-Spyder-1-310x116.png", 521, "Черный / Белый"));
+            items.Add(new ItemClass("Ferrari Портофино Россо", "/img/Ferrari-Portofino-1-310x133.png", 1014, "Красный"));
+            items.Add(new ItemClass("Mercedes GTS AMG", "/img/mercedes-gts-amg-310x135.png", 500, "Черный / Белый / Серый")); 
+                   }
+       
         public IActionResult Index()
         {
             return View();
@@ -30,6 +37,7 @@ namespace Сайт.Controllers
 
         public IActionResult Privacy()
         {
+            ViewBag.items = items;
             return View();
         }
 
---
+++ Models/ItemClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Сайт.Models
+{
+    public class ItemClass
+    {
+        public String Picture { get; set; }
+
+        public String Description { get; set; }
+        public String ColorC { get; set; }
+        public Decimal Price { get; set; }
+        public Boolean InStock { get; set; }
+
+        public ItemClass()
+        {
+            InStock = false;
+            Description = "Новый товар";
+            Price = 0;
+            ColorC ="ds ";
+        }
+
+        public ItemClass(String discription,String picrure, Decimal price, string ColorC): base()
+        {
+            this.Description = discription;
+            this.Picture = picrure;
+            this.Price = price;
+            this.ColorC = ColorC;
+
+        }
+    }
+}
